924ac19c666c870d8a6c01b999da30c38c8cfed5
saayan <saayan8981@gmail.com>
2020-08-06 10:57:09 +0530

    #4 validation script modified

--- DeadSystem/DeadContent_Validation/deadsystem_content_validation.py
+++ DeadSystem/DeadContent_Validation/deadsystem_content_validation.py
@@ -14,6 +14,10 @@ sys.setrecursionlimit(1500)
 class deadsystem_content_validation:
 
     retry_count=0
+    total_count = 0
+    pass_count = 0
+    fail_count = 0
+    code_exception = 0
 
     #initialization
     def __init__(self):
@@ -22,9 +26,6 @@ class deadsystem_content_validation:
         self.expired_token = 'Token token=0b4af23eaf275daaf41c7e57749532f128660ec3befa0ff3aee94636e86a43e7'
         self.source_array = ["amazon","netflix","tbs","nbc","tnt","hbomax","cbs","hbogo","hbonow","starz","showtime"]
         self.result = ''
-        self.total_count = 0
-        self.pass_count = 0
-        self.fail_count = 0
         self.csv_fieldnames = ["service_name","Url","source_id","Episode_title","Title","Show_type","Release_year","Season_number","Episode_number","Expiry_date","is_valid","modified_at","created_at","API_expiry_response","Result"]
 
     # Connection
@@ -39,9 +40,9 @@ class deadsystem_content_validation:
         self.expired_api='https://%s/expired_ott/is_available?source_program_id=%s&service_short_name=%s'
 
     #db query
-    def query(self,_id): 
+    def query(self,skip_id): 
         try:
-            self.query_result=self.sourcetable.aggregate([{"$match":{"$and":[{"item_type":{"$in":["movie","episode"]}},{"dump_date":self.current_date}]}},{"$skip":_id},{"$limit":100}])   
+            self.query_result=self.sourcetable.aggregate([{"$match":{"$and":[{"item_type":{"$in":["movie","episode"]}},{"dump_date":self.current_date}]}},{"$skip":skip_id},{"$limit":100}])   
             return self.query_result
         except pymongo.errors.OperationFailure as error:
             raise error
@@ -52,12 +53,14 @@ class deadsystem_content_validation:
                 self.retry_count = 0 
 
     def get_data_from_db(self,data):
-        self.service_name = data["source_id"]            
+        self.service_name = data["source_id"]
+        if self.service_name == "netflix":
+            self.service_name = "netflixusa"
         self.url = data["url"]
         self.item_type = data["item_type"]
         self.source_id = data["id"]
-        self.episode_title = data["episode_title"]
-        self.title = data["title"]
+        self.episode_title = unidecode.unidecode(pinyin.get(data["episode_title"]))
+        self.title = unidecode.unidecode(pinyin.get(data["title"]))
         self.release_year = data["release_year"]
         self.season_number = data["season_number"]
         self.episode_number = data["episode_number"]
@@ -71,7 +74,7 @@ class deadsystem_content_validation:
         self.get_env_url()
         self.mongo_connection()
         self.table = BeautifulTable()
-        self.table.column_headers = ["Total_tested", "Pass", "Fail"]
+        self.table.column_headers = ["Total test", "Test Pass", "Test Fail", "Code Exception/error"]
         self.logger = lib_common_modules().create_log(os.getcwd()+"/log/log.txt")
         result_sheet = '/output/Deadcontent_%s_validation%s.csv'%(thread_name,datetime.date.today())
         output_file = lib_common_modules().create_csv(result_sheet)
@@ -81,37 +84,45 @@ class deadsystem_content_validation:
             for _id in range(start_id,end_id,100):
                 db_result = self.query(_id)
                 for data in db_result:
-                    if data["source_id"] in self.source_array:
-                        self.total_count += 1
-                        self.logger.debug("\n")
-                        self.logger.debug(data)
-                        self.get_data_from_db(data)
-                        self.expiry_check = lib_common_modules().link_expiry_check(self.expired_api,self.domain,self.source_id,self.service_name,self.expired_token,self.logger)
-                        if self.expiry_check == 'True':
-                            self.result = "Pass"
-                            self.pass_count += 1
+                    try:
+                        if data["source_id"] in self.source_array:
+                            self.total_count += 1
+                            self.logger.debug("\n")
+                            self.logger.debug(data)
+                            self.get_data_from_db(data)
+                            self.expiry_check = lib_common_modules().link_expiry_check(self.expired_api,self.domain,self.source_id,self.service_name,self.expired_token,self.logger)
+                            if self.expiry_check == 'True':
+                                self.result = "Pass"
+                                self.pass_count += 1
+                            else:
+                                self.result = "Fail"
+                                self.fail_count += 1
+                            self.logger.debug("\n")    
+                            self.logger.debug([[self.service_name,self.url,self.source_id,self.episode_title,self.title,self.item_type,self.release_year,self.season_number,self.episode_number,self.expiry_date,self.is_valid,self.modified_at,self.created_at,self.expiry_check,self.result],thread_name,{"pass":self.pass_count},{"fail":self.fail_count},{"total":self.total_count}])    
+                            self.writer.writerow([self.service_name,self.url,self.source_id,self.episode_title,self.title,self.item_type,self.release_year,self.season_number,self.episode_number,self.expiry_date,self.is_valid,self.modified_at,self.created_at,self.expiry_check,self.result]) 
                         else:
-                            self.result = "Fail"
-                            self.fail_count += 1
-                        self.logger.debug("\n")    
-                        self.logger.debug([[self.service_name,self.url,self.source_id,self.episode_title,self.title,self.item_type,self.release_year,self.season_number,self.episode_number,self.expiry_date,self.is_valid,self.modified_at,self.created_at,self.expiry_check,self.result],thread_name])    
-                        self.writer.writerow([self.service_name,self.url,self.source_id,self.episode_title,self.title,self.item_type,self.release_year,self.season_number,self.episode_number,self.expiry_date,self.is_valid,self.modified_at,self.created_at,self.expiry_check,self.result]) 
-                    else:
+                            self.logger.debug("\n")
+                            self.logger.debug(["%s is not considerable for test"%data["source_id"],data["id"],data["title"],data["item_type"]])       
                         self.logger.debug("\n")
-                        self.logger.debug(["%s is not considerable for test"%data["source_id"],data["id"],data["title"],data["item_type"]])       
-                    self.logger.debug("\n")
-                    self.logger.debug(self.current_date) 
+                        self.logger.debug(self.current_date)
+                    except Exception as error:   
+                        self.code_exception += 1 
+                        self.logger.debug([{"error":error},{"data":data}]) 
+                        pass    
 
-            self.table.append_row([self.total_count, self.pass_count, self.fail_count])
+            self.table.append_row([self.total_count, self.pass_count, self.fail_count ,self.code_exception])
             self.logger.debug(self.table)
             self.connection.close()           
 
     #TODO: create threading
     def threading_pool(self):    
-        t1=Process(target=self.main,args=(0,"thread-1",1000))
+
+        t1 = Process(target=self.main, args=(0,"thread-1",500))
         t1.start()
-        t2=Process(target=self.main,args=(1000,"thread-2",2000))
+        t2 = Process(target=self.main, args=(500,"thread-2",1000))
         t2.start()
+        t3 = Process(target=self.main, args=(1000,"thread-3",1500))
+        t3.start()
 
 
 #Starting    
